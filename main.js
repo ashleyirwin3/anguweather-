ae5714e6dd14783681e266f27d4bd5dc6dd0263c
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-19 10:05:44 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+angular
+  .module('anguWeather', ['ngRoute'])
+  .config(($routeProvider) => {
+    $routeProvider
+      .when('/', {
+      controller: 'RootCtrl',
+      templateUrl: '/partials/root.html',
+    })
+  })
+  .controller('RootCtrl', function () {
+    console.log('I am a RootCtrl')
+  })
