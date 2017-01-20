f8761ff5a451daf10f59360eb6f70c33e4b60d71
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-20 09:24:21 -0600

    final commit

--- main.js
+++ main.js
@@ -3,10 +3,38 @@ angular
   .config(($routeProvider) => {
     $routeProvider
       .when('/', {
-      controller: 'RootCtrl',
-      templateUrl: '/partials/root.html',
-    })
+        controller: 'RootCtrl',
+        templateUrl: '/partials/root.html',
+      })
+      .when('/weather/:zipcode', {
+        controller: 'WeatherCtrl',
+        templateUrl: '/partials/weather.html',
+      })
   })
-  .controller('RootCtrl', function () {
+  .controller('RootCtrl', function ($scope, $location) {
     console.log('I am a RootCtrl')
+    $scope.gotoWeather = () => $location.url(`/weather/${$scope.zip}`)
+  })
+  .controller('WeatherCtrl', function ($scope, $routeParams, weatherFactory) {
+    console.log('I am a WeatherCtrl')
+
+    weatherFactory
+      .getWeather($routeParams.zipcode)
+      .then((weather) => {
+        $scope.temperature = weather.temp
+        $scope.city = weather.city
+      })
+  })
+  .factory('weatherFactory', ($http) => {
+    return {
+      getWeather (zipcode) {
+        return $http
+          .get(`http://api.wunderground.com/api/275dc60d2eae171d/conditions/q/${zipcode}.json`)
+          .then((response) => ({
+              temp: response.data.current_observation.temp_f,
+              city: response.data.current_observation.display_location.full,
+            })
+          )
+      },
+    }
   })
